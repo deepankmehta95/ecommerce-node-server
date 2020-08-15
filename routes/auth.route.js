eea250e39c08ae454bad433b747e7e820ea1b8d2
Deepank Mehta <deepankmehta@Deepanks-MacBook-Air.local>
2020-08-16 01:34:43 +0530

    Finished login and auth middleware

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,11 +3,26 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
-const {check, validationResult} = require('express-validator');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 const gravatar = require('gravatar');
 
+const auth = require('../middleware/auth');
+
 const User = require('../models/User');
-const { response } = require('express');
+
+// POST api/user
+router.post('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password').lean();
+        res.json(user);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error');
+    }
+});
 
 // POST api/user/register
 router.post('/register', [
@@ -25,20 +40,24 @@ router.post('/register', [
     }
 
     // Fetch form data
-    const {name, email, password} = req.body;
+    const {
+        name,
+        email,
+        password
+    } = req.body;
 
     try {
         // Check if user already exists
-        let user = await User.findOne({email});
+        let user = await User.findOne({
+            email
+        });
 
         // If user exists
         if (user) {
             return res.status(400).json({
-                errors: [
-                    {
-                        msg: 'User already exists'
-                    }
-                ]
+                errors: [{
+                    msg: 'User already exists'
+                }]
             });
         }
 
@@ -48,10 +67,13 @@ router.post('/register', [
             r: 'pg',
             d: 'mm'
         });
-        
+
         // Creates new user
         user = new User({
-            name, email, password, avatar
+            name,
+            email,
+            password,
+            avatar
         });
 
         // Encrypts the password
@@ -74,10 +96,81 @@ router.post('/register', [
                 expiresIn: 360000 // for development purpose only
             }, (err, token) => {
                 if (err) throw err;
-                res.json({token});
+                res.json({
+                    token
+                });
             }
         )
 
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// POST api/user/login
+router.post('/login', [
+    check('email', 'Please include a valid email address').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    // If no error
+    const {
+        email,
+        password
+    } = req.body;
+
+    try {
+        let user = await User.findOne({
+            email
+        });
+
+        // If no user
+        if (!user) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'Invalid credentials'
+                }]
+            });
+        }
+
+        // If user found
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        // Passwords don't match
+        if (!isMatch) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'Invalid credentials'
+                }]
+            });
+        }
+
+        // If password matches
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        console.log(payload);
+
+        jwt.sign(payload,
+            process.env.JWT_SECRET, {
+                expiresIn: 360000
+            }, (err, token) => {
+                if (err) throw err;
+                res.json({
+                    token
+                });
+            });
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Server error');
